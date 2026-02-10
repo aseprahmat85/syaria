d330a916cad1776860423672a65cfe8eb9305200
aseprahmat85 <asep.rahmat85@gmail.com>
2026-02-10 09:25:59 +0700

    Add files via upload

---
+++ AppSrv.bat
@@ -0,0 +1,4 @@
+REM "Mengubah apapun pada command line di bawah ini dapat menyebabkan aplikasi tidak berjalan sebagaimana mestinya.."
+
+cd "Application"
+"java.exe" -jar  "TMCSyaSrv.jar" -Xms512m -Xmx2048m
